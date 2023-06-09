67641c1cc2f8edbc80f8fc697f1e6d831b3fe620
rommel dalisay <rommelmdalisay@gmail.com>
2023-06-09 10:17:36 +0800

    update delete inner join

--- vb-mysql-s/Form1.Designer.vb
+++ vb-mysql-s/Form1.Designer.vb
@@ -22,10 +22,148 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.txtDescription = New System.Windows.Forms.TextBox()
+        Me.txtCost = New System.Windows.Forms.TextBox()
+        Me.txtSell = New System.Windows.Forms.TextBox()
+        Me.numQty = New System.Windows.Forms.NumericUpDown()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.txtSearch = New System.Windows.Forms.TextBox()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.numQty, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(111, 40)
+        Me.ComboBox1.MaxDropDownItems = 100
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 24)
+        Me.ComboBox1.TabIndex = 0
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(419, 172)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowHeadersWidth = 51
+        Me.DataGridView1.RowTemplate.Height = 24
+        Me.DataGridView1.Size = New System.Drawing.Size(670, 208)
+        Me.DataGridView1.TabIndex = 1
+        '
+        'txtDescription
+        '
+        Me.txtDescription.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtDescription.Location = New System.Drawing.Point(111, 112)
+        Me.txtDescription.Name = "txtDescription"
+        Me.txtDescription.Size = New System.Drawing.Size(153, 38)
+        Me.txtDescription.TabIndex = 2
+        '
+        'txtCost
+        '
+        Me.txtCost.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtCost.Location = New System.Drawing.Point(111, 172)
+        Me.txtCost.Name = "txtCost"
+        Me.txtCost.Size = New System.Drawing.Size(153, 38)
+        Me.txtCost.TabIndex = 3
+        '
+        'txtSell
+        '
+        Me.txtSell.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtSell.Location = New System.Drawing.Point(111, 233)
+        Me.txtSell.Name = "txtSell"
+        Me.txtSell.Size = New System.Drawing.Size(153, 38)
+        Me.txtSell.TabIndex = 4
+        '
+        'numQty
+        '
+        Me.numQty.Location = New System.Drawing.Point(111, 298)
+        Me.numQty.Name = "numQty"
+        Me.numQty.Size = New System.Drawing.Size(120, 22)
+        Me.numQty.TabIndex = 5
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(111, 344)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(248, 416)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(618, 34)
+        Me.TextBox1.TabIndex = 8
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(261, 325)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 9
+        Me.Button2.Text = "update"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(272, 374)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 10
+        Me.Button3.Text = "delete"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'txtSearch
+        '
+        Me.txtSearch.Font = New System.Drawing.Font("Microsoft Sans Serif", 19.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtSearch.Location = New System.Drawing.Point(419, 42)
+        Me.txtSearch.Name = "txtSearch"
+        Me.txtSearch.Size = New System.Drawing.Size(234, 45)
+        Me.txtSearch.TabIndex = 11
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(1144, 450)
+        Me.Controls.Add(Me.txtSearch)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.numQty)
+        Me.Controls.Add(Me.txtSell)
+        Me.Controls.Add(Me.txtCost)
+        Me.Controls.Add(Me.txtDescription)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.numQty, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents txtDescription As TextBox
+    Friend WithEvents txtCost As TextBox
+    Friend WithEvents txtSell As TextBox
+    Friend WithEvents numQty As NumericUpDown
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents txtSearch As TextBox
 End Class
--- vb-mysql-s/Form1.vb
+++ vb-mysql-s/Form1.vb
@@ -1,3 +1,169 @@
-﻿Public Class Form1
+﻿Imports MySql.Data.MySqlClient
+Public Class Form1
+    Dim conn As MySqlConnection = New MySqlConnection("Data Source=localhost;Database=db_sales2023;User=root;Password=;")
+    Public sql, sql1 As String
+    Public dbcomm As MySqlCommand
+    Public dbread As MySqlDataReader
+    Public dataAdapter As MySqlDataAdapter
+    Public ds As DataSet = New DataSet
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Try
+            sql = "SELECT i.item_id,i.description, s.quantity FROM item i INNER JOIN stock s ON(i.item_id = s.item_id)"
+            conn.Open()
+
+            dbcomm = New MySqlCommand(sql, conn)
+            dbread = dbcomm.ExecuteReader()
+            ComboBox1.ValueMember = "Value"
+            ComboBox1.DisplayMember = "Key"
+            While dbread.Read()
+                ComboBox1.Items.Add(dbread("item_id"))
+
+                'ComboBox1.Items.Add(New DictionaryEntry(dbread("description"), dbread("item_id")))
+            End While
+            dbread.Close()
+            sql = "SELECT i.item_id, i.description, i.cost_price,i.sell_price, s.quantity FROM item i INNER JOIN stock s ON(i.item_id = s.item_id)"
+            dataAdapter = New MySqlDataAdapter(sql, conn)
+
+            dataAdapter.Fill(ds, "item")
+            DataGridView1.DataSource = ds
+            DataGridView1.DataMember = "item"
+
+        Catch ex As MySqlException
+            MsgBox(ex.Message())
+        End Try
+
+        conn.Close()
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        Try
+            sql = "SELECT * FROM item i INNER JOIN stock s ON(i.item_id = s.item_id) where i.item_id = " & Val(ComboBox1.SelectedItem)
+
+            MsgBox(sql)
+            conn.Open()
+            dbcomm = New MySqlCommand(sql, conn)
+            dbread = dbcomm.ExecuteReader()
+            While dbread.Read()
+                txtDescription.Text = dbread("description")
+                txtCost.Text = dbread("cost_price")
+                txtSell.Text = dbread("sell_price")
+                numQty.Value = dbread("quantity")
+            End While
+
+
+        Catch ex As MySqlException
+            MsgBox(ex.Message())
+        End Try
+        dbread.Close()
+        conn.Close()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Try
+            'sql = $"UPDATE item SET description = '{txtDescription.Text}', cost_price = {Convert.ToDouble(txtCost.Text)}, sell_price = {CDbl(txtSell.Text)}, image_path = 'default.jpg' WHERE item_id = {Val(ComboBox1.SelectedItem)}"
+            sql = $"UPDATE item ,stock s  INNER JOIN item i ON(i.item_id = s.item_id) 
+                SET i.description = '{txtDescription.Text}', i.cost_price = {Convert.ToDouble(txtCost.Text)}, i.sell_price = {CDbl(txtSell.Text)}, s.quantity = {Val(numQty.Value)},i.image_path = 'default.jpg' WHERE i.item_id = {Val(ComboBox1.SelectedItem)} "
+            conn.Open()
+            MessageBox.Show("do you want to save", "new item", MessageBoxButtons.YesNoCancel)
+            If DialogResult.Yes Then
+                dbcomm = New MySqlCommand(sql, conn)
+                Dim i As Integer = dbcomm.ExecuteNonQuery
+                If (i > 0) Then
+                    MsgBox("item updated")
+                End If
+            End If
+            'sql = $"UPDATE stock SET quantity = {Val(numQty.Value)} WHERE item_id =  {Val(ComboBox1.SelectedItem)}"
+            'dbcomm = New MySqlCommand(sql, conn)
+            'Dim j As Integer = dbcomm.ExecuteNonQuery
+            'If (j > 0) Then
+            '    MsgBox("item updated")
+            'End If
+        Catch ex As MySqlException
+            MsgBox(ex.Message())
+        End Try
+        conn.Close()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Try
+            conn.Open()
+
+            'sql = $"DELETE i, s FROM item i INNER JOIN stock s ON (i.item_id = s.item_id) WHERE i.item_id = {Val(ComboBox1.SelectedItem)}"
+
+            sql = $"DELETE FROM item WHERE item_id ={Val(ComboBox1.SelectedItem)}"
+            dbcomm = New MySqlCommand(sql, conn)
+            Dim i As Integer = dbcomm.ExecuteNonQuery
+            If (i > 0) Then
+                MsgBox("item deleted")
+            End If
+            Dim sql2 = $"DELETE FROM stock where item_id = {Val(ComboBox1.SelectedItem)}"
+            dbcomm = New MySqlCommand(sql2, conn)
+            Dim j As Integer = dbcomm.ExecuteNonQuery
+            If (j > 0) Then
+                MsgBox("item deleted")
+            End If
+        Catch ex As MySqlException
+            MsgBox(ex.Message())
+        End Try
+
+        conn.Close()
+    End Sub
+
+    Private Sub txtSearch_TextChanged(sender As Object, e As EventArgs) Handles txtSearch.TextChanged
+
+        Dim itemName As String = txtSearch.Text
+        ds.Clear()
+
+        Try
+            sql = $"SELECT i.item_id, i.description, i.cost_price,i.sell_price, s.quantity FROM item i INNER JOIN stock s ON(i.item_id = s.item_id) WHERE i.description LIKE '%{itemName}%'"
+            TextBox1.Text = sql
+            conn.Open()
+            dataAdapter = New MySqlDataAdapter(sql, conn)
+
+            dataAdapter.Fill(ds, "searchItem")
+
+            DataGridView1.DataSource = ds
+            DataGridView1.DataMember = "searchItem"
+        Catch ex As MySqlException
+            MsgBox(ex.Message())
+        End Try
+
+        conn.Close()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim description As String = txtDescription.Text
+        Dim cost As Double = Convert.ToDouble(txtCost.Text)
+        Dim sell As Double = CDbl(txtSell.Text)
+
+        Try
+            sql = $"INSERT INTO item (description, cost_price, sell_price, image_path) VALUES ('{description}', {cost}, {sell}, 'default.jpg')"
+
+            conn.Open()
+            MessageBox.Show("do you want to save", "new item", MessageBoxButtons.YesNoCancel)
+            If DialogResult.Yes Then
+                dbcomm = New MySqlCommand(sql, conn)
+                Dim i As Integer = dbcomm.ExecuteNonQuery
+                If (i > 0) Then
+                    MsgBox("item saved")
+                End If
+            End If
+            sql = $"INSERT INTO stock (item_id, quantity) VALUES(last_insert_id(), {Val(numQty.Value)})"
+            TextBox1.Text = sql
+
+            dbcomm = New MySqlCommand(sql, conn)
+            Dim j As Integer = dbcomm.ExecuteNonQuery
+            If (j > 0) Then
+                MsgBox("stock saved")
+            End If
+
+        Catch ex As MySqlException
+            MsgBox(ex.Message())
+        End Try
+        conn.Close()
+    End Sub
+
+
 
 End Class
